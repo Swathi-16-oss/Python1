a8387f772bbd5d167e91b53e433bee2f10cf732a
Swathi-16-oss <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-09 17:12:57 +0530

    Create factorial.py
    take the number from the user by inbuilt function input() and to convert the string into number use the function int()
    if the number is 1 or o then print factorial is 1
    if the number is greater than  1 then   store the number in one variable such as user number and then multipy the number  with decrease the decrease the number by 1
    repeat same then print number

---
+++ factorial.py
@@ -0,0 +1,47 @@
+import time
+start_time=time.time()
+
+n=int(input("enter the number"))
+fact=1
+if(n==0):
+    print(fact)
+else:
+    for i in range(1,n+1):
+        fact=fact*i
+        print(fact)
+
+        
+
+n=int(input("enter the number"))
+i=1
+fact=1
+if(n==0):
+    print(fact)
+while(n>0):
+    fact=fact*i
+    i=i+1
+    if(i==n+2):
+        break
+    print(fact)
+
+    
+import math
+n=int(input("enter the number"))
+print(math.factorial(n))
+
+
+
+def fact(n):
+    if(n==1):
+        return n
+    else:
+        facto=n*fact(n-1)
+        return facto
+n=int(input("enter the number"))
+print(fact(n))
+
+
+print("....%s sec"%(time.time()- start_time))
+    
+
+
