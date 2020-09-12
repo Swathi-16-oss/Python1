5ce49617bf9c66cfcc1bbdb7b373cbf74d5e83ab
Swathi-16-oss <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-12 19:21:47 +0530

    Create combinations.py

---
+++ combinations.py
@@ -0,0 +1,16 @@
+def com(li):
+    if(len(li)==0):
+        return []
+    elif(len(li)==1):
+        return [li]
+    else:
+        for i in range(len(li)):
+            for j in range(len(li)):
+                            for k in range(len(li)):
+                                           if(i!=j & j!=k  &k!=i):
+                                               print(li[i],li[j])
+                           
+data=list('134')
+print('com')
+for j in com(data):
+    print(j)
