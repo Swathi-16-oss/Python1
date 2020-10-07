ecb45d16b5ffead9ce7db55da43905d3e471b09e
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-07 23:16:01 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -3,23 +3,28 @@ a=int(input("enter a number"))
 b=int(input("enter b number"))
 c=a+b
 print(c)
+'''o/p:
+enter a number5
+enter b number6
+11'''
+
 
 
 a,b=4,4
 c=a+b
 print(c)
-
-
+#0/p:8
 
 def add():
     return 4+3
 print(add())
+#o/p:7
 
 
 def add(a,b='default'):
         print(a+b)
 add(2,2)
-
+#o/p:4
 
 
 a=2
@@ -27,6 +32,11 @@ b=5
 c=a+b
 i="addition of {0} and {1} is {2}".format(a,b,c)
 print(i)
+
+'''o/p:
+addition of 2 and 5 is 7'''
+
+ 
 
 class add():
     def add1(self,a,b):
@@ -37,6 +47,11 @@ b=int(input("enter b num"))
 obj=add()
 c=obj.add1(a,b)
 print(c)
+''o/p:
+enter a num4
+enter b num5
+9'''
+
 
 
 class add():
@@ -49,8 +64,13 @@ b=int(input("enter b num"))
 obj=add()
 c=obj.add1(a,b)
 print(c)
+'''o/p:
+enter a num6
+enter b num7
+13'''
+
 
 a=(1,5)
 x=sum(a)
 print(x)
-
+#o/p:3
