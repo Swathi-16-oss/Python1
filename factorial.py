97a42e1fed94f7dd1c220f98b3ef9cd7e3877f5c
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-12-07 20:46:06 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -17,14 +17,16 @@ n=int(input("enter the number"))
 i=1
 fact=1
 if(n==0):
-    print(fact)
+    pass
 while(n>0):
     fact=fact*i
     i=i+1
-    if(i==n+2):
+    if(i==n+1):
         break
-    print(fact)
-    
+print(fact)
+'''o/p:
+enter the number5
+ 120''' 
 
 import math
 n=int(input("enter the number"))
@@ -35,9 +37,15 @@ def fact(n):
     else:
         facto=n*fact(n-1)
         return facto
+ '''o/p:
+ enter the number5
+ 120''' 
 n=int(input("enter the number"))
 print(fact(n))
 print("....%s sec"%(time.time()- start_time))
-    
+'''o/p:
+enter the number4
+24
+....3.894068479537964 sec '''   
 
 
