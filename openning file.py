b01b82c7966ce2c0e88f5dbdca17d64096f25a7b
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2021-01-06 23:27:10 +0530

    Update openning file.py

--- openning file.py
+++ openning file.py
@@ -7,8 +7,6 @@ f=open("C://Users/swath/OneDrive/Desktop/description.txt","r")
 for i in f:
     print(i)
 
-
-
 f=open("C://Users/swath/OneDrive/Desktop/description.txt","r")
 print(f.readline())#to read line by line in file 
 print(f.readline())
